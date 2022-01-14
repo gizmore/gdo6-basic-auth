c95d0c8f85933d590d8de49aa6890123895fb48c
gizmore <gizmore@wechall.net>
2022-01-14 22:52:27 +0100

    6.11.2-r6295_fixpack

--- Module_BasicAuth.php
+++ Module_BasicAuth.php
@@ -4,12 +4,16 @@ namespace GDO\BasicAuth;
 use GDO\Core\GDO_Module;
 use GDO\Core\GDT_Secret;
 use GDO\Core\Application;
+use GDO\DB\GDT_Checkbox;
+use GDO\User\GDO_User;
+use GDO\Login\Method\Form;
+use GDO\Util\Common;
 
 /**
  * BasicAuth module for gdo6.
  * 
  * @author gizmore
- * @since 6.11.1
+ * @since 6.11.3
  */
 final class Module_BasicAuth extends GDO_Module
 {
@@ -26,39 +30,83 @@ final class Module_BasicAuth extends GDO_Module
         return [
             GDT_Secret::make('basic_auth_user')->label('user_name'),
             GDT_Secret::make('basic_auth_pass')->label('password'),
+        	GDT_Checkbox::make('basic_authentication')->initial('1'),
+        	GDT_Checkbox::make('basic_auth_url')->initial('1'),
         ];
     }
     public function cfgUsername() { return $this->getConfigVar('basic_auth_user'); }
     public function cfgPassword() { return $this->getConfigVar('basic_auth_pass'); }
+    public function cfgAuthentication() { return $this->getConfigVar('basic_authentication'); }
+    public function cfgURL() { return $this->getConfigVar('basic_auth_url'); }
     
     ##################
     ### Middleware ###
     ##################
     public function onInit()
     {
-    	if (@$_SERVER['REQUEST_METHOD'] === 'OPTIONS')
+    	if ( (@$_SERVER['REQUEST_METHOD'] === 'OPTIONS') ||
+    	     (GDO_User::current()->isAuthenticated()) )
     	{
     		return;
     	}
+    	
     	if (Application::instance()->isWebServer())
     	{
+	    	$deny = true;
+
+    		if ($this->cfgURL())
+    		{
+    			$this->setupServerVarsFromURL();
+    		}
+    		
         	if ( ($username = $this->cfgUsername()) &&
         	     ($password = $this->cfgPassword()) )
         	{
         		if (!isset($_SERVER['PHP_AUTH_USER']))
         		{
-        			$this->deny();
         		}
         		elseif (strcasecmp($username, $_SERVER['PHP_AUTH_USER']) !== 0)
         		{
-        			$this->deny();
         		}
 	        	elseif (strcmp($password, $_SERVER['PHP_AUTH_PW']) !== 0)
 	        	{
-	        		$this->deny();
+	        	}
+	        	else
+	        	{
+	        		$deny = false;
 	        	}
         	}
+
+        	if ($deny && $this->cfgAuthentication() && module_enabled('Login'))
+        	{
+        		if (!isset($_SERVER['PHP_AUTH_USER']))
+        		{
+        		}
+        		else
+        		{
+        			$deny = !$this->tryAuthentication($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+        		}
+        	}
+        	if ($deny)
+        	{
+        		$this->deny();
+        	}
     	}
+    }
+    
+    private function setupServerVarsFromURL()
+    {
+    	$_SERVER['PHP_AUTH_USER'] = Common::getRequestString('xauth_user', @$_SERVER['PHP_AUTH_USER']);
+    	$_SERVER['PHP_AUTH_PW'] = Common::getRequestString('xauth_pw', @$_SERVER['PHP_AUTH_PW']);
+    }
+    
+    private function tryAuthentication($username, $password)
+    {
+    	if (!GDO_User::current()->isAuthenticated())
+    	{
+	    	Form::make()->onLogin($username, $password);
+    	}
+    	return GDO_User::current()->isAuthenticated();
     }
     
     private function deny()
